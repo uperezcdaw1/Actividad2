5751fe6da7aa7c173dc56dde577cdad1d49b70ec
Unai <uperezcdaw1@gmail.com>
2021-05-28 18:23:57 +0200

    Espacios insertados

--- PiedraPapelTijera/src/ende/Juego.java
+++ PiedraPapelTijera/src/ende/Juego.java
@@ -14,7 +14,7 @@ public class Juego
         Jugador p1=new Jugador(0,0);
         Jugador p2=new Jugador(0,0);
         boolean fin_de_juego=false;  
-        Integer Rondasjugadas = 0;    // Número de rondas jugadas
+        Integer Rondasjugadas = 0;    // Numero de rondas jugadas
         Integer EXITOS_jugador1=p1.Exitos;
         Integer EXITOS_jugador2=p2.Exitos;
         Integer EmPaTeS = 0;
@@ -30,31 +30,31 @@ public class Juego
             opcion_JUGADOR2 = p2.opcion_al_azar();
             System.out.println("Jugador 2: " + opcion_JUGADOR2+"\t Jugador 2 - Partidas ganadas: " + EXITOS_jugador2);
             
-            if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("papel"))){
+            if((opcion_JUGADOR1.equals("piedra")) && (opcion_JUGADOR2.equals("papel"))){
                 System.out.println("Jugador 2 GANA");
                 EXITOS_jugador2 = ++p2.Exitos;  
-            } else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("piedra"))){
+            } else if((opcion_JUGADOR1.equals("papel")) && (opcion_JUGADOR2.equals("piedra"))){
             	EXITOS_jugador1 = ++p1.Exitos;
                 System.out.println("Jugador 1 GANA");
-            } else if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("tijeras"))){
+            } else if((opcion_JUGADOR1.equals("piedra")) && (opcion_JUGADOR2.equals("tijeras"))){
             	EXITOS_jugador1 = ++p1.Exitos;
                 System.out.println("Jugador 1 GANA");
-            } else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals("piedra"))){
+            } else if((opcion_JUGADOR1.equals("tijeras")) && (opcion_JUGADOR2.equals("piedra"))){
             	EXITOS_jugador2 = ++p2.Exitos;
                 System.out.println("Jugador 2 GANA");
-            } else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals("papel"))){
+            } else if((opcion_JUGADOR1.equals("tijeras")) && (opcion_JUGADOR2.equals("papel"))){
             	EXITOS_jugador1 = ++p1.Exitos;
                 System.out.println("Jugador 1 GANA");
-            } else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("tijeras"))){
+            } else if((opcion_JUGADOR1.equals("papel")) && (opcion_JUGADOR2.equals("tijeras"))){
             	EXITOS_jugador2 = ++p2.Exitos;
                 System.out.println("Jugador 2 GANA");
             } 
-            if(opcion_JUGADOR1==opcion_JUGADOR2){
+            if(opcion_JUGADOR1 == opcion_JUGADOR2){
             	EmPaTeS++;
                 System.out.println("\n\t\t\t Empate \n");
             }
             Rondasjugadas++;
-            if((p1.Exitos>=3)||(p2.Exitos>=3)){
+            if((p1.Exitos>=3) || (p2.Exitos>=3)){
             	fin_de_juego=true;
                 System.out.println("FIN DEL JUEGO!!");
             }
@@ -67,7 +67,7 @@ public class Juego
  */
 class Jugador{
 	
-    int Exitos;      // número de partidas ganadas
+    int Exitos;      // numero de partidas ganadas
     int winTotal;
     public Jugador(int queExitos, int queWin) {
     	Exitos = queExitos;
