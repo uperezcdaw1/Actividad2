796f0def5512efb0f447440fa431bc61b320c989
Unai <uperezcdaw1@gmail.com>
2021-05-28 18:17:18 +0200

    Comentarios author y version

--- PiedraPapelTijera/src/ende/Juego.java
+++ PiedraPapelTijera/src/ende/Juego.java
@@ -2,6 +2,8 @@ package ende;
 
 /**
  * Simula el juego de Piedra, Papel o tijera
+ * @author Unai P
+ * @version 1.0
  */
 
 
@@ -28,45 +30,31 @@ public class Juego
             opcion_JUGADOR2 = p2.opcion_al_azar();
             System.out.println("Jugador 2: " + opcion_JUGADOR2+"\t Jugador 2 - Partidas ganadas: " + EXITOS_jugador2);
             
-            if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("papel")))
-            {
+            if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("papel"))){
                 System.out.println("Jugador 2 GANA");
-                EXITOS_jugador2 = ++p2.Exitos;
-                
-            }
-            else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("piedra")))
-            {
+                EXITOS_jugador2 = ++p2.Exitos;  
+            } else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("piedra"))){
             	EXITOS_jugador1 = ++p1.Exitos;
                 System.out.println("Jugador 1 GANA");
-            }
-            else if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("tijeras")))
-            {
+            } else if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("tijeras"))){
             	EXITOS_jugador1 = ++p1.Exitos;
                 System.out.println("Jugador 1 GANA");
-            }
-            else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals("piedra")))
-            {
+            } else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals("piedra"))){
             	EXITOS_jugador2 = ++p2.Exitos;
                 System.out.println("Jugador 2 GANA");
-            }
-            else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals("papel")))
-            {
+            } else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals("papel"))){
             	EXITOS_jugador1 = ++p1.Exitos;
                 System.out.println("Jugador 1 GANA");
-            }
-            else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("tijeras")))
-            {
+            } else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("tijeras"))){
             	EXITOS_jugador2 = ++p2.Exitos;
                 System.out.println("Jugador 2 GANA");
-            }
-            if(opcion_JUGADOR1==opcion_JUGADOR2)
-            {
+            } 
+            if(opcion_JUGADOR1==opcion_JUGADOR2){
             	EmPaTeS++;
                 System.out.println("\n\t\t\t Empate \n");
             }
             Rondasjugadas++;
-            if((p1.Exitos>=3)||(p2.Exitos>=3))
-            {
+            if((p1.Exitos>=3)||(p2.Exitos>=3)){
             	fin_de_juego=true;
                 System.out.println("FIN DEL JUEGO!!");
             }
